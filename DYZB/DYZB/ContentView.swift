666d22fa49d3ec274296ff599334ac7c864514fc
SSG <wwrssg@qq.com>
2019-10-03 23:12:37 +0800

    初始化项目

---
+++ DYZB/DYZB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DYZB
+//
+//  Created by 孙帅国 on 2019/10/3.
+//  Copyright © 2019 ssg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
